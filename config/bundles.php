8522f107b1b36c09683a305a9202faac5e47eea2
Jon González <jgonzalez@jamenet.eus>
2021-04-17 15:23:53 +0200

    Add api plataform

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,6 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];

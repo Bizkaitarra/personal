1e9e0bc6b1278bb6831f2e1053e268154451f2d3
Jon González <jgonzalez@jamenet.eus>
2021-04-18 17:09:01 +0200

    API documentation

---
+++ src/Shared/Domain/ExceptionResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Shared\Domain;
+
+
+class ExceptionResponse
+{
+    private string $message;
+
+    public function __construct(string $message)
+    {
+        $this->message = $message;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+
+
+
+}
